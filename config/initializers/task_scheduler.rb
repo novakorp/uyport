feb75cc53a2d230c237f5699dd2f573aa5a702fd
Dog <fedetob@gmail.com>
2016-04-20 22:03:19 -0300

    primer version github

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,6 +5,14 @@ scheduler = Rufus::Scheduler.new
 scheduler.every("3m") do   
    update_vehicle_positions
    puts "Fin Actualizacion .. "
+   
+end 
+
+scheduler2 = Rufus::Scheduler.new
+
+scheduler2.every("12h") do      
+     vh_save_odometer_readings   
+     puts "Fin actualizar lecturas Km"
 end 
 
 
